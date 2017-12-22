880c7f77c954d212e89ae23d1a2035788b7f7419
Joe Alden <joerobertalden@gmail.com>
2017-12-22 15:50:36 +0000

    Add react-slick CSS

--- src/html.js
+++ src/html.js
@@ -31,6 +31,16 @@ module.exports = class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <link rel="icon" type="image/png" href={favicon} />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+          />
           {this.props.headComponents}
           {css}
         </head>
